0e7bab4e9625eb43783147af742b8cafb70f6ab4
Ahmed2310Aligf <ahmedali231099@gmail.com>
2024-09-30 02:08:11 +0300

    Create Routes

---
+++ routes/bookRoutes.js
@@ -0,0 +1,13 @@
+
+const express = require('express');
+const router = express.Router();
+const bookController = require('../controllers/bookController');
+const auth = require('../middleware/auth');
+
+router.get('/', bookController.getBooks);
+router.get('/:id', bookController.getBookById);
+router.post('/', auth, bookController.createBook);
+router.put('/:id', auth, bookController.updateBook);
+router.delete('/:id', auth, bookController.deleteBook);
+
+module.exports = router;
